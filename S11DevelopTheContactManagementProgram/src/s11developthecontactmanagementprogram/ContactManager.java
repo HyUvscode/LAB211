50db33fc5254c8cfae9643cbc36c5efd325d1b28
HyU Hua <99528289+HyUvscode@users.noreply.github.com>
2023-06-07 09:32:11 +0700

    Update ContactManager.java

--- S11DevelopTheContactManagementProgram/src/s11developthecontactmanagementprogram/ContactManager.java
+++ S11DevelopTheContactManagementProgram/src/s11developthecontactmanagementprogram/ContactManager.java
@@ -9,11 +9,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
+ * This class manages the contacts.
  *
  * @author khuakhanhhuy
- */
-/**
- * This class manages the contacts.
  */
 public class ContactManager {
 
@@ -27,8 +25,16 @@ public class ContactManager {
         String name = Validation.getStringContact("Enter Name: ", "Error: The input name should only contain letters and spaces.");
         String group = Validation.getStringWithNumbers("Enter Group: ", "Error: The input group should only contain letters and number.");
         String address = Validation.getStringWithNumbers("Enter Address: ", "Error: The input address should only contain letters and number.");
-        String phone = Validation.getStringWithPhoneFormat("Enter Phone: ", "Note: Mobile phone numbers follow the format 09x-xxxxxxx or new prefixes: 03x, 05x, 07x, 08x.");
+        String phone;
+        // Check if the phone number is a duplicate
+        do {
+            phone = Validation.getStringWithPhoneFormat("Enter Phone: ", "Note: Mobile phone numbers follow the format 09x-xxxxxxx or new prefixes: 03x, 05x, 07x, 08x.");
 
+            // Check if the phone number is a duplicate
+            if (Validation.isDuplicatePhoneNumber(phone, contacts)) {
+                System.out.println("Error: Duplicate phone number. Please enter a different phone number.");
+            }
+        } while (Validation.isDuplicatePhoneNumber(phone, contacts));
         int id;
         if (contacts.isEmpty()) {
             id = 1; // If the contacts list is empty, set the ID as 1
@@ -90,24 +96,34 @@ public class ContactManager {
             System.out.println("No contacts available."); // Print a message if the contacts list is empty
             return; // Exit the method if there are no contacts available
         }
-        displayContacts(contacts);
+
         // Get the contact ID to delete from the user
-        int idFind = Validation.getInt("Enter the ID of the contact you want to delete: "); // Prompt the user for the ID of the contact to delete
-        boolean contactFound = false; // Flag to indicate if the contact with the specified ID is found
+        while (true) {
+            displayContacts(contacts);
+            // Get the contact ID to delete from the user
+            int idFind = Validation.getInt("Enter the ID of the contact you want to delete: "); // Prompt the user for the ID of the contact to delete
+            boolean contactFound = false; // Flag to indicate if the contact with the specified ID is found
 
-        // Iterate over the contacts and remove the contact with the specified ID
-        for (Iterator<Contact> iterator = contacts.iterator(); iterator.hasNext();) {
-            Contact contact = iterator.next();
-            if (contact.getId() == idFind) { // Check if the contact's ID matches the specified ID
-                iterator.remove(); // Remove the contact from the list
-                System.out.println("Contact " + idFind + " deleted successfully!"); // Print a success message
-                contactFound = true; // Set the flag to indicate that the contact was found
-                break; // Exit the loop since the contact has been found and removed
+            // Iterate over the contacts and remove the contact with the specified ID
+            for (Iterator<Contact> iterator = contacts.iterator(); iterator.hasNext();) {
+                Contact contact = iterator.next();
+                if (contact.getId() == idFind) { // Check if the contact's ID matches the specified ID
+                    iterator.remove(); // Remove the contact from the list
+                    System.out.println("Contact " + idFind + " deleted successfully!"); // Print a success message
+                    contactFound = true; // Set the flag to indicate that the contact was found
+                    break; // Exit the loop since the contact has been found and removed
+                }
             }
-        }
 
-        if (!contactFound) {
-            System.out.println("No contact found with ID " + idFind); // Print a message if no contact was found with the specified ID
+            if (!contactFound) {
+                System.out.println("No contact found with ID " + idFind); // Print a message if no contact was found with the specified ID
+
+                if (!Validation.checkInputYN()) {
+                    break; // Exit the loop if the user doesn't want to continue searching
+                }
+            } else {
+                break; // Exit the loop if the contact was found and deleted
+            }
         }
     }
 }
